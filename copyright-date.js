9812d04f387435b1a83fc61c4ba244d88b17d99f
Jorge C. <jacortez_94@hotmail.com>
2025-05-20 15:31:44 -0700

    Create copyright-date.js

---
+++ copyright-date.js
@@ -0,0 +1,28 @@
+class CopyrightYear {
+  construction(_yearContainer) {
+    this.yearContainer = _yearContainer;
+  }
+
+  init() {
+    if (!this.yearContainer) return;
+    const today = new Date();
+    let year = d.getFullYear();
+    
+    this.yearContainer.innerHTML = year;
+  }
+}
+
+const initializeCopyrightYear = () => 
+    window.skyscripts = window.skyscripts || [];
+    const triggers = document.querySelectorAll('[sr-copyright="container"]');
+    triggers.forEach(trigger => {
+        let instance = new CopyrightYear(trigger);
+        window.skyscripts.push({'CopyrightYear': instance});
+    });
+}
+
+if (/complete|interactive|loaded/.test(document.readyState)) {
+    initializeCopyrightYear();
+} else {
+    window.addEventListener('DOMContentLoaded', initializeCopyrightYear);
+}
